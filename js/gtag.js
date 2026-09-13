b25b32158c465badf8dc6773ad654452b2ebe4ad
ahmedgad 112 <gadoo007551@gmail.com>
2026-09-14 01:55:48 +0300

    اضافة جوجيل بيكسل

---
+++ js/gtag.js
@@ -0,0 +1,15 @@
+window.dataLayer = window.dataLayer || [];
+function gtag() {
+  window.dataLayer.push(arguments);
+}
+
+(function loadGoogleTag() {
+  var script = document.createElement('script');
+  script.async = true;
+  script.src = 'https://www.googletagmanager.com/gtag/js?id=AW-18396653825';
+  document.head.appendChild(script);
+
+  gtag('js', new Date());
+  gtag('config', 'AW-18396653825');
+  gtag('config', 'GT-NS433Z574');
+})();
